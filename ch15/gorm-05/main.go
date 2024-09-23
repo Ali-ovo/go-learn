17a799ffcc6790a704dcb9b711a5a29f963aca09
ali <ali_ovo@qq.com>
2024-09-23 20:43:31 +0800

    feat: add gorm demo

---
+++ ch15/gorm-05/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type User struct {
+	ID           uint           // Standard field for the primary key
+	Name         string         // 一个常规字符串字段
+	Email        *string        // 一个指向字符串的指针, allowing for null values
+	Age          uint8          // 一个未签名的8位整数
+	Birthday     *time.Time     // A pointer to time.Time, can be null
+	MemberNumber sql.NullString // Uses sql.NullString to handle nullable strings
+	ActivatedAt  sql.NullTime   // Uses sql.NullTime for nullable time fields
+	CreatedAt    time.Time      // 创建时间（由GORM自动管理）
+	UpdatedAt    time.Time      // 最后一次更新时间（由GORM自动管理）
+}
+
+func main() {
+	dsn := "root:123456@tcp(192.168.189.128:3306)/gorm_test?charset=utf8mb4&parseTime=True&loc=Local"
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,        // Don't include params in the SQL log
+			Colorful:                  true,        // Disable color
+		},
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	var user User
+	result := db.Last(&user)
+	fmt.Println(user)
+	fmt.Println(result.RowsAffected)
+	errors.Is(result.Error, gorm.ErrRecordNotFound)
+
+	// 主键查询
+	db.First(&user, 5)
+	fmt.Println(user)
+
+	var users []User
+	results := db.Find(&users)
+	fmt.Println("总条数:", results.RowsAffected)
+
+	for _, user := range users {
+		fmt.Println(user)
+	}
+}
---
+++ ch15/gorm-06/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type User struct {
+	ID           uint           // Standard field for the primary key
+	Name         string         // 一个常规字符串字段
+	Email        *string        // 一个指向字符串的指针, allowing for null values
+	Age          uint8          // 一个未签名的8位整数
+	Birthday     *time.Time     // A pointer to time.Time, can be null
+	MemberNumber sql.NullString // Uses sql.NullString to handle nullable strings
+	ActivatedAt  sql.NullTime   // Uses sql.NullTime for nullable time fields
+	CreatedAt    time.Time      // 创建时间（由GORM自动管理）
+	UpdatedAt    time.Time      // 最后一次更新时间（由GORM自动管理）
+}
+
+func main() {
+	dsn := "root:123456@tcp(192.168.189.128:3306)/gorm_test?charset=utf8mb4&parseTime=True&loc=Local"
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,        // Don't include params in the SQL log
+			Colorful:                  true,        // Disable color
+		},
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	var user User
+	// db.Where("name=?", "ali").First(&user)
+
+	db.Where(&User{Name: "ali"}).First(&user)
+
+	fmt.Println(user)
+}
