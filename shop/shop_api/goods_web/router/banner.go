c3475e202e9cac690bd829947057ef6777466d75
Ali <ali_ovo@qq.com>
2024-10-10 17:36:42 +0800

    feat: add braner and brand

---
+++ shop/shop_api/goods_web/router/banner.go
@@ -0,0 +1,19 @@
+package router
+
+import (
+	"go-learn/shop/shop_api/goods_web/api/banners"
+	"go-learn/shop/shop_api/goods_web/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
+
+func InitBannerRouter(Router *gin.RouterGroup) {
+	BannerRouter := Router.Group("banners")
+
+	{
+		BannerRouter.GET("", banners.List)                                                            // 轮播图列表页
+		BannerRouter.DELETE("/:id", middlewares.JWTAuth(), middlewares.IsAdminAuth(), banners.Delete) // 删除轮播图
+		BannerRouter.POST("", middlewares.JWTAuth(), middlewares.IsAdminAuth(), banners.New)          //新建轮播图
+		BannerRouter.PUT("/:id", middlewares.JWTAuth(), middlewares.IsAdminAuth(), banners.Update)    //修改轮播图信息
+	}
+}
