dbce6b600a66da91c6dad1e4ff75c85c4a029436
ali <ali_ovo@qq.com>
2024-11-07 20:15:27 +0800

    feat: add error demo

--- ch19/error/main.go
+++ ch19/error/main.go
@@ -1,5 +1,37 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/pkg/errors"
+)
+
+func (s *Student) SetName(name string) error {
+	if name == "" || s.Name == "" {
+		return errors.New("name can't be empty")
+	}
+	s.Name = name
+	return nil
+}
+
+type Student struct {
+	Name string
+	Age  int
+}
+
+func NewStudent() (*Student, error) {
+	stu := &Student{
+		Age: 18,
+	}
+
+	e := stu.SetName("")
+	if e != nil {
+		return stu, errors.Wrap(e, "set name")
+	}
+	return stu, nil
+}
+
+func main() {
+	_, e := NewStudent()
+	fmt.Printf("%+v\n", e)
 }
